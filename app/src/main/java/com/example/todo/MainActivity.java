7eae183933070f3700a82578f72f529f95b500f7
Rophy Tsai <rophyt@amazon.com>
2021-01-04 12:30:03 +0800

    add amplify live sync code

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.AWSDataStorePlugin;
 import com.amplifyframework.datastore.generated.model.Priority;
@@ -16,34 +17,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
+            Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSDataStorePlugin());
             Amplify.configure(getApplicationContext());
 
             Log.i("Tutorial", "Initialized Amplify");
-        } catch (AmplifyException e) {
-            Log.e("Tutorial", "Could not initialize Amplify", e);
+        } catch (AmplifyException failure) {
+            Log.e("Tutorial", "Could not initialize Amplify", failure);
         }
-        Amplify.DataStore.query(
-                Todo.class,
-                todos -> {
-                    while (todos.hasNext()) {
-                        Todo todo = todos.next();
-
-                        Log.i("Tutorial", "==== Todo ====");
-                        Log.i("Tutorial", "Name: " + todo.getName());
-
-                        if (todo.getPriority() != null) {
-                            Log.i("Tutorial", "Priority: " + todo.getPriority().toString());
-                        }
-
-                        if (todo.getDescription() != null) {
-                            Log.i("Tutorial", "Description: " + todo.getDescription());
-                        }
-                    }
-                },
-                failure -> Log.e("Tutorial", "Could not query DataStore", failure)
-        );
-        super.onCreate(savedInstanceState);
+        Amplify.DataStore.observe(Todo.class,
+                started -> Log.i("Tutorial", "Observation began."),
+                change -> Log.i("Tutorial", change.item().toString()),
+                failure -> Log.e("Tutorial", "Observation failed.", failure),
+                () -> Log.i("Tutorial", "Observation complete.")
+        );        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     }
